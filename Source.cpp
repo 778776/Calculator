06be04f3d851d3b86bdc20188c6e19211da2f473
Khachatur <x.alanyan@gmail.com>
2019-03-01 01:09:51 +0400

    Second Commit

--- Source.cpp
+++ Source.cpp
@@ -2,8 +2,6 @@
 #include <string>
 
 
-
-
 class Parser {
 public:
 	std::string mem1; std::string mem2; char oper;
@@ -27,36 +25,36 @@ public:
 		else {
 			oper = a[pos];
 			for (int i = 0; i < pos; i++) {
-				mem1 += a[i];
+				mem1 += a.substr(i, 1);
 			}
 			for (int j = pos + 1; j < size; j++) {
-				mem2 += a[j];
+				mem2 += a.substr(j, 1);
 			}
 		}
 
 	}
 };
 
-class getErrors :public Parser {
+class getErrors {
 public:
 	std::string mem1, mem2, errorLine;
 	void getErors(std::string a, std::string b) {
 		int size1 = a.size();
 		int size2 = b.size();
 		for (int i = 0; i < size1; i++) {
-			if (a[i] == '0' || a[i] == '1' || a[i] == '2' || a[i] == '3' || a[i] == '4' || a[i] == '5' || a[i] == '6' || a[i] == '7' || a[i] == '8' || a[i] == '9' || a[i] == ' ') {
-				mem1 += a[i];
+			if (a[i] == '.' || a[i] == '0' || a[i] == '1' || a[i] == '2' || a[i] == '3' || a[i] == '4' || a[i] == '5' || a[i] == '6' || a[i] == '7' || a[i] == '8' || a[i] == '9' || a[i] == ' ') {
+				mem1 += a.substr(i, 1);
 			}
 			else {
-				errorLine += a[i];
+				errorLine += a.substr(i, 1);
 			}
 		}
 		for (int i = 0; i < size2; i++) {
-			if (b[i] == '0' || b[i] == '1' || b[i] == '2' || b[i] == '3' || b[i] == '4' || b[i] == '5' || b[i] == '6' || b[i] == '7' || b[i] == '8' || b[i] == '9' || b[i] == ' ') {
-				mem2 += b[i];
+			if (b[i] == '.' || b[i] == '0' || b[i] == '1' || b[i] == '2' || b[i] == '3' || b[i] == '4' || b[i] == '5' || b[i] == '6' || b[i] == '7' || b[i] == '8' || b[i] == '9' || b[i] == ' ') {
+				mem2 += b.substr(i, 1);
 			}
 			else {
-				errorLine += b[i];
+				errorLine += b.substr(i, 1);
 			}
 		}
 	}
@@ -80,31 +78,29 @@ int main() {
 			std::cout << "please correct your command (" << d.errorLine << ")" << std::endl;
 		}
 		else if (d.mem1.size() > 0 && d.mem2.size() > 0) {
-			int mem1 = stoi(d.mem1);
-			int mem2 = stoi(d.mem2);
-			int result;
-			if (b.oper == '+') {
+			float mem1 = stof(d.mem1);
+			float mem2 = stof(d.mem2);
+			float result;
+			switch (oper) {
+			case '+':
 				result = mem1 + mem2;
-			}
-			else if (b.oper == '-') {
+				break;
+			case '-':
+				break;
 				result = mem1 - mem2;
-			}
-			else if (b.oper == '*') {
-				result = mem1 * mem2;
-			}
-			else if (b.oper == '/') {
+				break;
+			case '/':
 				result = mem1 / mem2;
+				break;
+			case '*':
+				result = mem1 * mem2;
+				break;
 			}
-
-			std::cout << mem1 << b.oper << mem2 << "=" << result << std::endl;
-		}
-		else {
-			std::cout << "please write your command" << std::endl;
+			std::cout << mem1 << oper << mem2 << " = " << result << std::endl;
 		}
 	}
 	system("pause");
 	return 0;
 }
-
 
 
