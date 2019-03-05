928d426ad085540a931702996ddc656de367d60c
Khachatur <x.alanyan@gmail.com>
2019-03-06 01:17:55 +0400

    Tokenizer

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
+#include <vector>
 #include <string>
-
 
 
 
@@ -27,84 +27,84 @@ public:
 		else {
 			oper = a[pos];
 			for (int i = 0; i < pos; i++) {
-				mem1 += a[i];
+				mem1 += a.substr(i);
 			}
 			for (int j = pos + 1; j < size; j++) {
-				mem2 += a[j];
+				mem2 += a.substr(j);
 			}
 		}
-
 	}
 };
 
-class getErrors :public Parser {
+
+
+class getErrors {
 public:
-	std::string mem1, mem2, errorLine;
-	void getErors(std::string a, std::string b) {
-		int size1 = a.size();
-		int size2 = b.size();
-		for (int i = 0; i < size1; i++) {
-			if (a[i] == '0' || a[i] == '1' || a[i] == '2' || a[i] == '3' || a[i] == '4' || a[i] == '5' || a[i] == '6' || a[i] == '7' || a[i] == '8' || a[i] == '9' || a[i] == ' ') {
-				mem1 += a[i];
-			}
-			else {
-				errorLine += a[i];
-			}
-		}
-		for (int i = 0; i < size2; i++) {
-			if (b[i] == '0' || b[i] == '1' || b[i] == '2' || b[i] == '3' || b[i] == '4' || b[i] == '5' || b[i] == '6' || b[i] == '7' || b[i] == '8' || b[i] == '9' || b[i] == ' ') {
-				mem2 += b[i];
-			}
-			else {
-				errorLine += b[i];
+	std::string errorLine;
+	std::vector<std::string> mem;
+	void getErors(std::vector<std::string> mem) {
+		for (int j = 0; j < mem.size(); j++)
+		{
+			for (int i = 0; i < mem[j].size(); i++) {
+				if (mem[j][i] == '.' || mem[j][i] == '0' || mem[j][i] == '1' || mem[j][i] == '2' || mem[j][i] == '3' || mem[j][i] == '4' || mem[j][i] == '5' || mem[j][i] == '6' || mem[j][i] == '7' || mem[j][i] == '8' || mem[j][i] == '9' || mem[j][i] == ' ') {
+					mem[j] += mem[j][i];
+				}
+				else {
+					errorLine += mem[j][i];
+				}
 			}
 		}
 	}
 };
 
-
-int main() {
-
-	std::cout << "enter your command" << std::endl;
-	for (int i = 1; i < 10; i++) {
-		std::cout << i << ")";
-		std::string a;
-		std::getline(std::cin, a);
-		Parser b;
-		b.Parse(a);
-		char oper = b.oper;
-
-		getErrors d;
-		d.getErors(b.mem1, b.mem2);
-		if (d.errorLine.size() > 0) {
-			std::cout << "please correct your command (" << d.errorLine << ")" << std::endl;
+	std::vector<std::string> tokenizer(std::string a) {
+		int size = a.size();
+		int token_count = 0;
+		
+		int j = 1;
+		for (int i = 0; i < size; i++) {
+			if (a[i] == '+' || a[i] == '-' || a[i] == '*' || a[i] == '/') {
+				token_count++;
+			}
 		}
-		else if (d.mem1.size() > 0 && d.mem2.size() > 0) {
-			int mem1 = stoi(d.mem1);
-			int mem2 = stoi(d.mem2);
-			int result;
-			if (b.oper == '+') {
-				result = mem1 + mem2;
+		std::vector<int> pos(token_count + 2);
+		for (int i = 0; i < size; i++) {
+			if (a[i] == '+' || a[i] == '-' || a[i] == '*' || a[i] == '/') {
+				pos[j] = i;
+				j++;
 			}
-			else if (b.oper == '-') {
-				result = mem1 - mem2;
-			}
-			else if (b.oper == '*') {
-				result = mem1 * mem2;
-			}
-			else if (b.oper == '/') {
-				result = mem1 / mem2;
-			}
-
-			std::cout << mem1 << b.oper << mem2 << "=" << result << std::endl;
 		}
-		else {
-			std::cout << "please write your command" << std::endl;
+		pos[0] = 0;
+		std::vector<std::string> mem(token_count + 1);
+		mem[0] = a.substr(0, pos[1]);
+		for (int i = 1; i <= token_count; i++) {
+			mem[i] = a.substr(pos[i] + 1, pos[i + 1] - pos[i] - 1);
+		}
+		return mem;
+	}
+
+	int main() {
+
+		std::cout << "enter your command" << std::endl;
+		for (int i = 1; i < 10; i++) {
+			std::cout << i << ")";
+			std::string a;
+			std::getline(std::cin, a);
+			std::vector<std::string> mem = tokenizer(a);
+			getErrors d;
+			d.getErors(mem);
+			mem = d.mem;
+			if (d.errorLine.size() > 0) {
+				std::cout << "please correct your command (" << d.errorLine << ")" << std::endl;
+			}
+			else
+				for (int i = 0; i < mem.size(); i++)
+				{
+					std::cout << mem[i] << std::endl;
+				}
+
+			system("pause");
+			return 0;
 		}
 	}
-	system("pause");
-	return 0;
-}
-
-
 
