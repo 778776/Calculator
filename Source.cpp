e117f871530008b7bfbc1f4613a205eca8addaa4
Khachatur <x.alanyan@gmail.com>
2019-03-30 00:55:51 +0400

    First

--- Source.cpp
+++ Source.cpp
@@ -2,28 +2,54 @@
 #include <string>
 #include <vector>
 
-std::vector<std::string> tokenizer(std::string a) {
-	std::vector<std::string> b;
-	int last_pos = 0;
-	for (int i = 0; i < a.size(); i++) {
-		if (a[i] == '+' || a[i] == '-' || a[i] == '/' || a[i] == '*') {
-			b.push_back(a.substr(last_pos, i - last_pos));
-			b.push_back(a.substr(i, 1));
-			last_pos = i + 1;
+class Calculator
+{
+public:
+	void evaluate(const std::string & a)
+	{
+		// TODO: clean whitespaces from tokenize
+		// TODO: fix/remove empty tokens
+		tokenize(a);
+		for (auto& a : m_tokens)
+		{
+			std::cout << a << "  ";
 		}
+		// TODO: implement validate()
+		validate();
+		// evaluate();
 	}
-	b.push_back(a.substr(last_pos, a.size() - last_pos));
-	return b;
-}
 
-int main() {
+	void validate() {
+		// 3a + 5 + c -> !ERROR< 3a >! + 5 + !ERROR< c >!
+	}
+	void tokenize(const std::string & a)
+	{
+		int last_pos = 0;
+		for (int i = 0; i < a.size(); i++)
+		{
+			if (a[i] == '+' || a[i] == '-' || a[i] == '/' || a[i] == '*')
+			{
+				m_tokens.push_back(a.substr(last_pos, i - last_pos));
+				m_tokens.push_back(a.substr(i, 1));
+				last_pos = i + 1;
+			}
+		}
+		m_tokens.push_back(a.substr(last_pos, a.size() - last_pos));
+	}
+
+private:
+	std::vector < std::string > m_tokens{};
+
+};
+
+
+int main()
+{
 	std::string a;
 	std::getline(std::cin, a);
-	std::vector<std::string> mem = tokenizer(a);
-	for (int i = 0; i < mem.size(); i++)
-	{
-		std::cout << mem[i] << "  ";
-	}
+
+	Calculator c;
+	c.evaluate(a);
 	system("pause");
 	return 0;
 }
