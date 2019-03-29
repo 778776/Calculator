9d60cbe26243cfd92ec59db9e482b30bde7809a5
Khachatur <x.alanyan@gmail.com>
2019-03-30 00:14:29 +0400

    tokenize

--- Source.cpp
+++ Source.cpp
@@ -2,13 +2,13 @@
 #include <string>
 #include <vector>
 
-std::vector<std::string> tokenize(std::string a) {
+std::vector<std::string> tokenizer(std::string a) {
 	std::vector<std::string> b;
 	int last_pos = 0;
 	for (int i = 0; i < a.size(); i++) {
 		if (a[i] == '+' || a[i] == '-' || a[i] == '/' || a[i] == '*') {
 			b.push_back(a.substr(last_pos, i - last_pos));
-			b.push_back(a[i];
+			b.push_back(a.substr(i, 1));
 			last_pos = i + 1;
 		}
 	}
@@ -19,12 +19,11 @@ std::vector<std::string> tokenize(std::string a) {
 int main() {
 	std::string a;
 	std::getline(std::cin, a);
-	std::vector<std::string> mem = tokenize(a);
-	for (int i = 0; i < mem.size() - 1; i++)
+	std::vector<std::string> mem = tokenizer(a);
+	for (int i = 0; i < mem.size(); i++)
 	{
-		std::cout << mem[i] << oper[i];
+		std::cout << mem[i] << "  ";
 	}
 	system("pause");
 	return 0;
 }
-
